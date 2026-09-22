c27602f01c1123ac011026cde9f4e5b2bb3648bb
Gabriel Stolfa <gabriel.stolfa@gmail.com>
2026-09-21 21:09:55 -0300

    Remove agendamento interno de notificacoes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,43 +1,16 @@
 require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
-const cron = require('node-cron')
 const router = require('./routes')
-const eventService = require('./services/eventService')
-const pushService = require('./services/pushService')
 const pool = require('./database')
 
 const app = express()
+
 app.use(express.json())
 app.use(cors())
 app.use(router)
 
-cron.schedule('0 8 * * *', async () => {
-
-  const event = eventService()
-
-  if (!event) {
-    console.log('Nenhum evento!')
-    return
-  }
-
-  console.log('Evento encontrado!', event)
-
-  try {
-
-    await pushService(event)
-
-    console.log('Notificação automática enviada!')
-
-  } catch (error) {
-
-    console.log('Erro ao enviar notificação:', error.message)
-
-  }
-
-}, {
-  timezone: 'America/Sao_Paulo'
-})
 const PORT = process.env.PORT || 3000
 
 pool.query('SELECT NOW()')
@@ -49,4 +22,6 @@ pool.query('SELECT NOW()')
     console.log('Erro ao conectar no banco:', error.message)
   })
 
-app.listen(PORT, ()=> console.log('Servidor iniciado!'))
+app.listen(PORT, () => {
+  console.log('Servidor iniciado!')
+})
