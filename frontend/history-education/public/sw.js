207e154b3651c5d7681d291686fd04d5453e0ba6
Gabriel Stolfa <gabriel.stolfa@gmail.com>
2026-09-21 21:01:55 -0300

    Corrige clique da notificacao

--- frontend/history-education/public/sw.js
+++ frontend/history-education/public/sw.js
@@ -18,9 +18,11 @@ self.addEventListener('push', (event) => {
 })
 
 self.addEventListener('notificationclick', (event) => {
+  console.log('🖱️ NOTIFICAÇÃO CLICADA!')
+
   event.notification.close()
 
   event.waitUntil(
-   self.clients.openWindow('https://hoje-na-historia.onrender.com')
+    self.clients.openWindow('https://hoje-na-historia.onrender.com')
   )
 })
