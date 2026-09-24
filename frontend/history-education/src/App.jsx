73a38f01012d9d6b2e0a5d006809b8ff05b91663
Gabriel Stolfa <gabriel.stolfa@gmail.com>
2026-09-24 19:46:55 -0300

    Adiciona loading

--- frontend/history-education/src/App.jsx
+++ frontend/history-education/src/App.jsx
@@ -1,48 +1,51 @@
 import { useEffect, useState } from "react"
 import Card from "./components/card/Card"
 
-
 export default function App() {
+  const [prev, setPrev] = useState('')
+  const [fullEvent, setFullEvent] = useState('')
+  const [loading, setLoading] = useState(true)
 
-    const [prev, setPrev] =  useState('')
-    const [fullEvent, setFullEvent] = useState('')
-    const [loading, setLoading] = useState(false)
-
-  
-
-    useEffect(()=> {
-       async function getEvent() {
-        try {
-        setLoading(true)
-        const response = await fetch('https://hoje-na-historia-api.onrender.com/events')
+  useEffect(() => {
+    async function getEvent() {
+      try {
+        const response = await fetch(
+          'https://hoje-na-historia-api.onrender.com/events'
+        )
 
         if (!response.ok) {
-        throw new Error('Erro ao buscar evento')
-      }
+          throw new Error('Erro ao buscar evento')
+        }
+
         const data = await response.json()
+
         setPrev(data.event.preview)
         setFullEvent(data.event.fullEvent)
-        } catch (error) {
-          setPrev(error.message)
-          setFullEvent('')
-        } finally {
-          setLoading(false)
-        }
-        
+
+      } catch (error) {
+        setPrev(error.message)
+        setFullEvent('')
+
+      } finally {
+        setLoading(false)
+      }
     }
 
-      getEvent()
-    },[])
+    getEvent()
+  }, [])
 
-  return(
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p>Carregando evento de hoje...</p>
+      </div>
+    )
+  }
 
-
-    <>
-      {loading && <p>Buscando...</p>}
-      <Card
-        prev={prev}
-        fullEvent={fullEvent}
-      />  
-    </>
+  return (
+    <Card
+      prev={prev}
+      fullEvent={fullEvent}
+    />
   )
 }
